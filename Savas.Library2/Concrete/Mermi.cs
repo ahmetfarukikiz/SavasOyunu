f7f9c429c535581d8ae6a193c43170e76ec9c405
ahmetfarukikiz <ahmetfarukikiz41@gmail.com>
2025-07-03 04:30:45 +0300

    deneysel bi takım şeyler denedim artık mermiler giderken animasyonlu gibi davranıyor ama optimizasyonunu yapamadığım için muhtemelen silicem

--- Savas.Library2/Concrete/Mermi.cs
+++ Savas.Library2/Concrete/Mermi.cs
@@ -1,12 +1,21 @@
 ﻿using Savas.Library2.Abstract;
+using Timer = System.Windows.Forms.Timer;
 
 namespace Savas.Library2.Concrete
 {
     class Mermi : Cisim
     {
+        private static readonly string[] mermiResimleri = { "mermi1", "mermi2", "mermi3" };
+        private int mermiResmiIndexi = 0;
+        private readonly Timer _mermiResimTimer = new Timer { Interval = 50 };
+
         public Mermi(Size hareketAlaniBoyutlari, int namluOrtasiX) : base(hareketAlaniBoyutlari)
         {
             BaslangicKonumunuAyarla(namluOrtasiX);
+
+            _mermiResimTimer.Tick += MermiResimTimer_Tick;
+            _mermiResimTimer.Start();
+
         }
 
         private void BaslangicKonumunuAyarla(int namluOrtasiX)
@@ -14,6 +23,16 @@ namespace Savas.Library2.Concrete
             Bottom = HareketAlaniBoyutlari.Height;
             Center = namluOrtasiX;
             HareketMesafesi = (int)(Height * 1.5); 
+        }
+
+        private void MermiResimTimer_Tick(object sender, EventArgs e)
+        {
+         
+            Image = Image.FromFile($@"images\{mermiResimleri[mermiResmiIndexi]}.png");
+            if(mermiResmiIndexi < 2) mermiResmiIndexi++;
+            if (mermiResmiIndexi == 2) mermiResmiIndexi = 0;
+
+
         }
     }
 }
